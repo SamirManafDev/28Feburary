33fdad1caacd239350604c8d2ebf180ee466861e
SamirManafDev <samirmanafli161@gmail.com>
2023-03-01 15:18:53 +0400

    Task 28 feburary

---
+++ Task28_Feb/Task28_Feb/Program.cs
@@ -0,0 +1,24 @@
+﻿//Task 28 Feburary
+
+
+//string[] animals = { "dog", "rabbit", "fox", "chicken" };
+//for (int i = 0; i < animals.Length; i++)
+//{
+//    if (animals[i].Length > 4)
+//    {
+//        Console.WriteLine(animals[i]);
+//    }
+
+//}
+
+
+
+//int[] num = { 1, 3, 4, 5 };
+//int sum=0;
+//for (int i = 0; i < num.Length; i++)
+//{
+//    sum += num[i];
+
+//}
+//Console.WriteLine(sum);
+
